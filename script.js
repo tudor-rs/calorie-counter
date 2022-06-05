4e6bf98bdf1449518cdbba9946986f03e5cd9719
Tudor Rusu <rusutudor@protonmail.com>
2022-06-05 18:48:01 +0300

    Removed back button and replaced it with dynamic add/return button

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const header = document.querySelector('.header');
 const body = document.querySelector('.body');
+const messageBox = document.querySelector('.message-box');
 const counter = document.createElement('input');
 counter.type = 'number';
 counter.inputMode = 'numeric';
 counter.id = 'counter';
 body.appendChild(counter);
-const addBtn = document.querySelector('#add-btn');
-const backBtn = document.querySelector('#back-btn');
+const footer = document.querySelector('.footer');
+const buttonAdd = document.querySelector('#button-add');
+const buttonBack = document.createElement('div');
+buttonBack.classList.add('button');
+buttonBack.id = 'button-back';
+buttonBack.innerText = 'X';
 
 let currentCalories = 0;
 let lastAccessed;
@@ -58,18 +63,24 @@ counter.addEventListener('change', () => {
 
 // add functionality
 
-addBtn.addEventListener('click', () => {
+buttonAdd.addEventListener('click', () => {
     body.removeChild(counter);
-
     const addCalories = document.createElement('input');
     addCalories.type = 'number';
     addCalories.inputMode = 'numeric';
     addCalories.id = 'addCalories';
     body.appendChild(addCalories);
+    messageBox.innerText = 'Calories to add:';
+    footer.removeChild(buttonAdd);
+    footer.appendChild(buttonBack);
+    addCalories.focus();
 
     addCalories.addEventListener('change', () => {
         currentCalories = currentCalories + parseFloat(addCalories.value);
         body.removeChild(addCalories);
+        footer.removeChild(buttonBack);
+        footer.appendChild(buttonAdd);
+        messageBox.innerText = '';
         counter.value = currentCalories;
         body.appendChild(counter);
         localStorage.setItem("calories", currentCalories);
@@ -80,9 +91,19 @@ addBtn.addEventListener('click', () => {
     })
 });
 
-backBtn.addEventListener('click', () => {
+// return functionality
+
+buttonBack.addEventListener('click', () => {
     while (body.lastChild) {
         body.removeChild(body.lastChild);
     }
+    footer.removeChild(buttonBack);
+    body.appendChild(messageBox);
     body.appendChild(counter);
+    footer.appendChild(buttonAdd);
+
+    messageBox.innerText = '';
 });
+
+
+// add hold functionality instead of click on circle?
